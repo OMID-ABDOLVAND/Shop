67b496fb5538ac579b91db48cf6fce6641f475d8
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2023-12-17 07:25:17 -0800

    feature: add category to serializer for Admin

---
+++ catalog/serializers/admin.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+
+from catalog.models import Category
+
+
+class CreateCategoryNodeSerializer(serializers.ModelSerializer):
+    parent = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'title', 'description', 'is_public', 'slug', 'parent')
+
+    def create(self, validated_data):
+        parent = validated_data.pop('parent', None)
+        if parent is None:
+            instance = Category.add_root(**validated_data)
+        else:
+            parent_node = get_object_or_404(Category, pk=parent)
+            instance = parent_node.add_child(**validated_data)
+        return instance
+
+
+class CategoryTreeSerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    def get_children(self, obj):
+        return CategoryTreeSerializer(obj.get_children(), many=True).data
+
+    class Meta:
+        model = Category
+        fields = ('id', 'title', 'description', 'is_public', 'slug', 'parent')
